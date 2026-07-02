e168a636ddb6abef572a20ae0f83e602534c9643
Shadia Noor Mou <shadianoormou.cse@gmail.com>
2026-07-02 23:19:59 +0600

    Add Group Anagrams solution

---
+++ strings/group_anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+
+            groups[tuple(count)].append(word)
+
+        return list(groups.values())
