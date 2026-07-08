00698493bd35553d8b72cf10bc53f7c5061ca547
Shadia Noor Mou <shadianoormou.cse@gmail.com>
2026-07-08 22:31:55 +0600

    Create daily_temperatures.py

---
+++ stacks/daily_temperatures.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+
+            stack.append(i)
+
+        return result
+
