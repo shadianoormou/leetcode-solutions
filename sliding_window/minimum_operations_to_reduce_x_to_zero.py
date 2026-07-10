d4ecd7fbfb2ad05e76275c6163e4056ecaba7185
Shadia Noor Mou <shadianoormou.cse@gmail.com>
2026-07-10 22:12:35 +0600

    Create minimum_operations_to_reduce_x_to_zero.py

---
+++ sliding_window/minimum_operations_to_reduce_x_to_zero.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        target = sum(nums) - x
+
+        if target < 0:
+            return -1
+
+        if target == 0:
+            return len(nums)
+
+        left = 0
+        current_sum = 0
+        max_length = -1
+
+        for right in range(len(nums)):
+            current_sum += nums[right]
+
+            while current_sum > target:
+                current_sum -= nums[left]
+                left += 1
+
+            if current_sum == target:
+                max_length = max(max_length, right - left + 1)
+
+        if max_length == -1:
+            return -1
+
+        return len(nums) - max_length
