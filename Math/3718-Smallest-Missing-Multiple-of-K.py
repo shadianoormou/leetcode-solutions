8788079c478e39aa558945c4210fd7cd883736eb
Shadia Noor Mou <shadianoormou.cse@gmail.com>
2026-08-25 21:08:07 +0600

    Implement function to find smallest missing multiple of K

---
+++ Math/3718-Smallest-Missing-Multiple-of-K.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        seen = set(nums)
+        multiple = k
+
+        while multiple in seen:
+            multiple += k
+
+        return multiple
